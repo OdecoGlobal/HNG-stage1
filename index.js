46a65b7a123a03782437077f2784429723946ac1
Okechukwu Chidera <okechukwuchidera12@gmail.com>
2023-09-08 12:14:10 +0100

    updated Javascript

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const day = document.querySelector(".day");
 const time = document.querySelector(".time");
 
-const now = new Date();
 // console.log(now);
 const weekDay = [
   "Sunday",
@@ -13,10 +12,20 @@ const weekDay = [
   "Friday",
   "Saturday",
 ];
-day.textContent = weekDay[now.getDay()];
 
 const updateDayAndTime = function () {
+  const now = new Date();
+
+  // update Day
+  day.textContent = weekDay[now.getDay()];
+  //  update Time
   time.textContent = `${now.getTime()} ms`;
 };
 updateDayAndTime();
 setInterval(updateDayAndTime, 1000);
+
+const timer = () => {
+  console.log(new Date().getTime());
+};
+
+// setInterval(timer, 1000);
